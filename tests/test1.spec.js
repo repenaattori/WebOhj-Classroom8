ac401703eae34411a0b1da11918a3edaf2d5c214
reimarii <reimarii@oamk.fi>
2025-08-14 09:59:47 +0300

    init

---
+++ tests/test1.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { log } from 'node:console';
+import exp from 'node:constants';
+
+test('Test fetch cocktail info', async ({ page }) => {
+    await page.goto(`file://${process.cwd()}/index.html`);
+
+    let input = page.locator('#id');
+    let button = page.getByRole('button');
+
+    let requestPromise = page.waitForResponse('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007');
+    await input.fill('11007');
+    await button.click();
+    await requestPromise;
+    await expect(page.locator('h3')).toHaveText(/Margarita/);
+    await expect(page.locator('h3')).toHaveText(/Ordinary Drink/);
+
+    requestPromise = page.waitForResponse('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=17222');
+    await input.fill('17222');
+    await button.click();
+    await requestPromise;
+    await expect(page.locator('h3')).toHaveText(/A1/);
+    await expect(page.locator('h3')).toHaveText(/Cocktail/);
+});
---
+++ tests/test2.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { log } from 'node:console';
+import exp from 'node:constants';
+
+test('Test fetch cocktail image', async ({ page }) => {
+    await page.goto(`file://${process.cwd()}/index.html`);
+
+    let input = page.locator('#id');
+    let button = page.getByRole('button');
+
+    let requestPromise = page.waitForResponse('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007');
+    await input.fill('11007');
+    await button.click();
+    await requestPromise;
+    await expect(page.locator('img')).toHaveAttribute('src', new RegExp('https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg'));
+
+    requestPromise = page.waitForResponse('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=17222');
+    await input.fill('17222');
+    await button.click();
+    await requestPromise;
+    await expect(page.locator('img')).toHaveAttribute('src', new RegExp('https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg'));
+});
